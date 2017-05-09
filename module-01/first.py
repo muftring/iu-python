f143ec6675f3e06f2f3f716a5f73048ce4897b56
Michael Uftring <muftring@indiana.edu>
2017-05-09 15:19:20 -0400

    first.py

---
+++ module-01/first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 12:17:02 2017
+
+@author: michael
+"""
+
+def hello():
+    print("hello, michael")
+    
+def greet(person):
+    print("hello,", person)
+    
+hello()
+greet("alisa")
