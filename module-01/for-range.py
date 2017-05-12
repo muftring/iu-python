fc9725691754216ef85b6f22163692b2ef46ab3c
Michael Uftring <muftring@indiana.edu>
2017-05-12 12:07:41 -0400

    some Python for loops and use of range()

---
+++ module-01/for-range.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 12 10:42:21 2017
+
+@author: michael
+"""
+
+print("print range(5)")
+for i in range(5):
+    print(i)
+
+print("print first five positive integers")
+for i in range(5):
+    print(i+1)
+
+print("print first 5 even integers, excluding 0")
+for i in range(5):
+    print((i+1)*2)
