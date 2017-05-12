21e489e447e0c65d1bf691363776995e8f6260a0
Michael Uftring <muftring@indiana.edu>
2017-05-12 16:04:49 -0400

    assignment 1 source files

---
+++ module-01/assignment-1-2.x.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 12 12:07:59 2017
+
+@author: michael
+"""
+
+pi = 3.14159
+
+def circleArea(r):
+    print(pi * r ** 2)
+    
+circleArea(2)
+
+def computeAverage():
+    print((10+20+30+40)/4)
+    
+computeAverage()
+
+kmPerMile = 0.62
+
+def kilometersToMiles(km):
+    print(km*kmPerMile)
+    
+kilometersToMiles(600)
+
+def squareArea(side):
+    print(side * side)
+    
+squareArea(12)
---
+++ module-01/assignment-1-3.x.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 12 12:24:05 2017
+
+@author: michael
+"""
+
+def firstFiveSquares():
+    for i in range(1,6,1):
+        print(i**2)
+        
+firstFiveSquares()
+
+def firstTenReverse():
+    for i in range(10,0,-1):
+        print(i*2)
+        
+firstTenReverse()
