87c7c11abe8356ae63e2147052f0beda037f656a
Michael Uftring <muftring@persistent.com>
2017-05-21 17:47:01 -0400

    implement a2-q4: fuel efficiency

--- module-02/Question4.py
+++ module-02/Question4.py
@@ -3,4 +3,19 @@
 #
 # Assignment 2, Question 4
 #
-# 
+# From a U.S. fuel economy (miles/gallon) value, compute and display 
+# the European fuel consumption (L/100km).
+
+kmPerMile = 1.6
+litersPerGallon = 3.785
+
+def fuelEff():
+    milesPerGallon = eval(input("Please enter mileage (miles per gallon): "))
+    litersPer100Km = 1 / ((milesPerGallon * kmPerMile) * (1 / litersPerGallon)) * 100
+    print("Vehicle consumption is",milesPerGallon,"miles per gallon.")
+    print("Vehicle economy is ",litersPer100Km,"liters per 100 km.")
+
+def main():
+    fuelEff()
+
+main()
