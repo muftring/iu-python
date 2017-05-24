cd866fe8d326a299353046a84f3e270287f16913
Michael Uftring <muftring@persistent.com>
2017-05-24 13:50:50 -0400

    Assignment 3, Question 1 - improvements: remove pritning table while in compute(), check that start<=end, add notes

--- module-03/Question1.py
+++ module-03/Question1.py
@@ -8,26 +8,29 @@
 # Given a user-provided inclusive range of integer values,
 # calculate and display the ratio of the sum of even numbers to
 # the sum of all the numbers in the range.
+#
+# Notes:
+#  (1) there is no protection against division by zero in compute()
+#  (2) ranges which include negative numbers will work
 # 
-
 def compute(start, end): 
     all = 0
     even = 0
-    print("\ni\teven\tall")
-    print("---\t----\t---")
     for i in range(start, end+1, 1):
         all += i
         if (i % 2 == 0):
             even += i
-        print(i, "\t", even, "\t", all)
     return even/all
 
 def main():
     print("Range sum(even) to sum(all) ratio computer!")
-    start = int(input("Enter the start number of range: "))
-    end = int(input("Enter the end number of range: "))
-    ratio = compute(start, end)
-    print("\nThe ratio of the sume of all even numbers to the sum of all numbers is:")
-    print(ratio)
+    start = int(input("Enter the start number of the range: "))
+    end = int(input("Enter the end number of the range: "))
+    if (start <= end):
+        ratio = compute(start, end)
+        print("\nThe ratio of the sum of all even numbers to the sum of all numbers in the range", start, "to", end, "is:")
+        print(ratio)
+    else:
+        print("\nError - the range `start` value must be less than or equal to the `end` value")
     
 main()
