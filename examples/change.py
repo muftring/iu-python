502698447ca5fda23ad763ab98f91f757783519d
Michael Uftring <muftring@persistent.com>
2017-05-24 13:24:27 -0400

    example: change.py ~ calculate dollars+cents from user provided number of coins

---
+++ examples/change.py
@@ -0,0 +1,14 @@
+# change.py
+#   A program to calculate the value of some change in dollars
+def main():
+    print("Change Counter")
+    print()
+    print("Please enter the count of each coin type.")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+    total = quarters * .25 + dimes * .10 + nickels * .05 + pennies * .01
+    print()
+    print("The total value of your change is", total)
+main()
