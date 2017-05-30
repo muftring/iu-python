6a4c6ff8a3f4a20335f053eddb5d50195e68d18b
Michael Uftring <muftring@persistent.com>
2017-05-30 16:07:19 -0400

    Assignment 4, Question 1 - print a string in reverse order

---
+++ module-04/Question1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Michael Uftring, Indiana University
+# I590 - Python, Summer 2017
+#
+# Assignment 4, Question 1
+#
+# Write a program that takes an input string from the user and prints 
+# the string in a reverse order.
+#
+
+def main():
+    forward = input("Please enter a string: ")
+    reverse = ""
+    for i in range(1, len(forward)+1, 1):
+        reverse += forward[-1*i]
+    print("The string",forward,"in reverse is",reverse)
+    
+main()
