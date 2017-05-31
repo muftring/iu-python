618ce394034f733b3641978112aa4c09251bbf0e
Michael Uftring <muftring@persistent.com>
2017-05-31 11:25:47 -0400

    Assignmane 4, Question 4: determine if a string is a palindrome

--- module-04/Question4.py
+++ module-04/Question4.py
@@ -5,3 +5,17 @@
 #
 # Assignment 4, Question 4
 #
+# Determines if the string is a palindrome, and show an appropriate message.
+#
+
+def main():
+    forward = input("Enter a string to check: ")
+    reverse = ""
+    for i in range(1, len(forward)+1, 1):
+        reverse += forward[-1*i]
+    if (forward == reverse):
+        print("The string is a palindrome.")
+    else:
+        print("he string is not a palindrome.")
+    
+main ()
