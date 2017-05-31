26c83cb75eb0c3d64d31bbc207416ccf74af0f08
Michael Uftring <muftring@persistent.com>
2017-05-31 11:19:42 -0400

    Assignment 4, Question 3: calculate the numeric value of a name

--- module-04/Question3.py
+++ module-04/Question3.py
@@ -5,3 +5,19 @@
 #
 # Assignment 4, Question 3
 #
+# Write a program that calculates the numeric value of a single name 
+# provided as input. This will be accomplished by summing up the values 
+# of the letters of the name where ’a’ is 1, ’b’ is 2, ’c’ is 3 etc., 
+# up to ’z’ being 26. 
+#
+
+base = ord("a")-1
+
+def main():
+    sum = 0
+    name = input("Enter any name in lower case: ")
+    for letter in name:
+        sum += (ord(letter) - base)
+    print("The numeric value of entered name is", sum)
+    
+main()
