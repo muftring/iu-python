c62c7200f25ef5399e62c56cbebc91c3aa1c3d29
Michael Uftring <muftring@persistent.com>
2017-05-31 11:34:32 -0400

    Assignment 4, Question 5: count the number of words in a file

--- module-04/Question5.py
+++ module-04/Question5.py
@@ -5,3 +5,18 @@
 #
 # Assignment 4, Question 5
 #
+# Count and print the number of words in a file.
+#
+
+def main():
+    wordCount = 0
+    fileName = input("Please enter a file name: ")
+    inputFile = open(fileName, "r")
+    for line in inputFile.readlines():
+        words = line.split()
+        wordCount += len(words)
+    inputFile.close()
+    result = "There are {0} words in the file.".format(wordCount)
+    print(result)
+
+main()
