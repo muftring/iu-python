1821adba8f93e31579f414a4955400128da2a29d
Michael Uftring <muftring@persistent.com>
2017-06-30 12:45:00 -0400

    Assignment 9 - initial files

---
+++ module-09/Question.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+# Michael Uftring, Indiana University
+# I590 - Python, Summer 2017
+#
+# Assignment 9
+#
+
+import numpy as np
+
+def main():
+    print("")
+
+if __name__ == '__main__':
+    main()
