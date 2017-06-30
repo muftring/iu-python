b5de050f681c94ef1a15162b99c82faed11ada29
Michael Uftring <muftring@persistent.com>
2017-06-30 12:44:24 -0400

    examples: some NumPy exploration

---
+++ examples/numpy-explore.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+
+import numpy as np
+
+list1 = [1, 2.5, 0, 23]
+array1 = np.array(list1)
+
+print("list1:",list1)
+print("array1:",array1)
+
+list2 = [[1, 2, 3], [4, 5, 6]]
+array2 = np.array(list2)
+
+print("list2:",list2)
+print("array2:",array2)
